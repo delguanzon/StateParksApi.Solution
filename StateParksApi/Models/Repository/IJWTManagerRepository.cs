f33f4263c41e9c5127cfa465cc93bc77c4b324d6
delguanzon <yodelguanzon@gmail.com>
2023-01-22 00:18:55 -0800

    Add JWTRepository Interface and Implementation

---
+++ StateParksApi/Models/Repository/IJWTManagerRepository.cs
@@ -0,0 +1,14 @@
+using StateParksApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateParksApi.Repository
+{
+    public interface IJWTManagerRepository
+    {
+        Tokens Authenticate(User user); 
+    }
+
+}
