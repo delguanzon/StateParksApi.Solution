972d63ce35bbabde75416d645a3f8d520d7d1d30
delguanzon <yodelguanzon@gmail.com>
2023-01-21 21:42:21 -0800

    Add versioning package Add versioning to builder services

--- StateParksApi/Program.cs
+++ StateParksApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using StateParksApi.Models;
@@ -17,6 +18,15 @@ builder.Services.AddDbContext<StateParkDbContext>(dbContextOptions  =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApiVersioning(opt =>
+                                    {
+                                        opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1,0);
+                                        opt.AssumeDefaultVersionWhenUnspecified = true;
+                                        opt.ReportApiVersions = true;
+                                        opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                                                        new HeaderApiVersionReader("x-api-version"),
+                                                                                        new MediaTypeApiVersionReader("x-api-version"));
+                                    });
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
